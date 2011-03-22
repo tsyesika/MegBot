cfaba1ece6edc1db38d9d1b1f5198cba8618670d
Jessica Tallon <xray7224@googlemail.com>
2011-03-22 12:03:50 +0000

    Fixed with http links

--- Plugins/dent.py
+++ Plugins/dent.py
@@ -1,15 +1,18 @@
-import re, urllib2
+import re, urllib2, traceback
 def main(connection, line):
 	if len(line.split()) <= 3:
-		connection.core["privmsg"].main(connection, line.split()[2], "Please put a username")
+		line = line + " " + line.split()[0].split("!")[1:]
 	else:
 		try:
-			i = urllib2.urlopen("http://identi.ca/xray7224")
+			i = urllib2.urlopen("http://identi.ca/%s" % line.split()[-1])
 			data = i.read()
 			last = re.findall("<p class=\"entry-content\">(.+?)</p>", data)[0]
 			name = re.findall("<title>(.+?) ", data)[0]
 			time = re.findall(">about (.+?) ago</abbr>", data)[0]
+			#checks for url's 
+			last = re.sub("<a href=\"(.+?)\".*?>.*?<\/a>", "\g<1>", last)
 			connection.core["privmsg"].main(connection, line.split()[2], "\002[%s]\017 %s - \002Aprox: %s\017" % (name, last, time))
 		except:
+			traceback.print_exc()
 			connection.core["privmsg"].main(connection, line.split()[2], "An error has occured")
 		
