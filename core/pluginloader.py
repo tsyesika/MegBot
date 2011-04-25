183fe570f3b4619b9980d3b32c9b13b626222e14
Jessica Tallon <xray7224@googlemail.com>
2011-04-24 19:09:27 -0700

    Fixes folder name to put it to uppercase

--- core/pluginloader.py
+++
@@ -1,5 +0,0 @@
-import glob, imp
-
-def main(connection):
-	for g in glob.glob(connection.config.paths["plugin"] + "*.py"):
-		connection.plugins[g.replace(connection.config.paths["plugin"], "").replace(".py", "")] = imp.load_source(g.replace(connection.config.paths["plugin"], "").replace("*.py", ""), g)
--- core/privmsg.py
+++
@@ -1,7 +0,0 @@
-def main(connection, channel, message):
-	""" Sends a PRIVMSG """
-	connection.core["raw"].main(connection, "PRIVMSG %s :%s" % (channel, message))
-
-def initalise():
-	""" Called when plugin is loaded """
-	pass
