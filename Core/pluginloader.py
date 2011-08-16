aaa02f4b26831b3fb6c65e1fb62e29616b8b15aa
Jessica T <xray7224@googlemail.com>
2011-08-16 01:23:00 +0100

    fixes hooker

--- Core/pluginloader.py
+++ Core/pluginloader.py
@@ -3,3 +3,6 @@ import glob, imp
 def main(connection):
 	for g in glob.glob(connection.config.paths["plugin"] + "*.py"):
 		connection.plugins[g.replace(connection.config.paths["plugin"], "").replace(".py", "")] = imp.load_source(g.replace(connection.config.paths["plugin"], "").replace("*.py", ""), g)
+	for plugin in connection.plugins.keys():
+		if "init" in dir(connection.plugins[plugin]):
+			connection.plugins[plugin].init(connection)
