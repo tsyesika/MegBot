cfc812a1c30d7616440858040e4268fc26bb1f45
Jessica T <xray7224@googlemail.com>
2011-08-25 02:11:02 +0100

    Changes from eval to google (Big security fix)

--- Plugins/math.py
+++ Plugins/math.py
@@ -15,50 +15,30 @@
 #   along with MegBot.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
-from __future__ import division
-from math import *
-from fractions import gcd
-import random, traceback
+import urllib2, urllib, re
 
-def lcm(a,b):
-    return a*b/gcd(a,b)
-def round(a):
-	return int(a + 0.5)
+def fixurl(url):
+	url = url.replace("+", "%2B")
+	url = url.replace("/", "%2F")
+	url = url.replace("(", "%28")
+	url = url.replace(")", "%29")
+	url = url.replace(" ", "+")
+	return url
+
 def main(connection, line):
-	random.seed()
 	if len(line.split()) <= 3:
 		connection.core["privmsg"].main(connection, line.split()[2], "Plese enter mathamatical expression.")
 		return
-	
-	exp = " ".join(line.split()[4:]).replace("^", "**").replace("x", "*")
-	
-	##
-	# constants
-	##
-	ln = log1p
-	lg = lambda x:log(x, 2)
-	log = lambda x:log(x, 10)
-	rand = random.randint(0, 100)
-	##
-	# End of constants
-	##
-	
+	exp = " ".join(line.split()[4:])
+	google = urllib2.Request("http://google.com/m?q=%s" % fixurl(exp))
+	google.add_header("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-us) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3")
+	google = urllib2.urlopen(google)
+	source = google.read()
+	print source
+	a = re.findall("<div class=\"wansgk\"><span class=\"sifhoi\">(.+?)</span> </div> <div>", source)
 	try:
-		a = eval(exp)
-	except ZeroDivisionError:
-		connection.core["privmsg"].main(connection, line.split()[2], "Math Error: Zero Division Error.")
-		return
+		connection.core["privmsg"].main(connection, line.split()[2], "%s" % a[0])
 	except:
 		connection.core["privmsg"].main(connection, line.split()[2], "Math Error: ?")
-		traceback.print_exc()
-		return
-		
-	##
-	# Andreas work below
-	##
-	if a % 1 == 0:
-		a = int(a)
-	connection.core["privmsg"].main(connection, line.split()[2], "Answer: %s" % a)
-		
 		
 	
