60246ec4a92bb3b1e9dba39d28d5facb3970fa8b
Jessica T <xray7224@googlemail.com>
2012-01-24 00:27:33 +0000

    Fixes identica plugin (and now uses API)

--- Plugins/dent.py
+++ Plugins/dent.py
@@ -15,31 +15,25 @@
 #   along with MegBot.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
-import re, urllib2, traceback
+import json, urllib2, traceback
 
 def main(connection, line):
+	print "being called here"
 	if len(line.split()) <= 4:
 		line = line + " " + line.split()[0].split("!")[0][1:]
 	try:
-		i = urllib2.urlopen("http://identi.ca/%s" % line.split()[-1])
+		i = urllib2.urlopen("http://identi.ca/api/statuses/user_timeline.json?screen_name=%s" % line.split()[-1])
 		data = i.read()
-		last = re.findall("<p class=\"entry-content\">(.+?)</p>", data)[0]
-		name = re.findall("<title>(.+?) ", data)[0]
-		time = re.findall("> a (.+?) ago </addr>", data)
-		if not time:
-			time = re.findall(">about (.+?) ago</abbr>", data)[0]
+		data = json.loads(data)
+		if data:
+			# this is from the http://status.net/wiki/Twitter-compatible_API
+			name = data[0]["user"]["screen_name"]
+			cid = data[0]["id"]
+			status = data[0]["text"]
+			time = data[0]["created_at"]
+			connection.core["privmsg"].main(connection, line.split()[2], "\002[%s]\017 %s - \002Aprox: %s\017 - http://www.identi.ca/notice/%s" % (name, status, time, cid))
 		else:
-			time = "a %s ago" % time[0]
-		identification = re.findall("<li class=\"hentry notice\" id=\"notice-(.+?)\">", data)[0]
-		
-		#Fixes tagging
-		last = re.sub("<span class=\"tag\"><a href=.*? rel=\"tag\">(.+?)</a></span>", "\g<1>", last)
-		#checks for url's
-		last = re.sub("<span class=\"vcard\">.*?<span class=\"fn nickname\">(.+?)</span></a></span>", "\g<1>", last)
-		last = re.sub("<a href=\"(.+?)\".*?>.*?<\/a>", "\g<1>", last)
-		last = re.sub("<span class=\"(.+?)\".*?>.*?<\/span>", "", last)
-		last = last.replace("&quot;", "\"").replace("&gt;", ">").replace("&lt;", "<")
-		connection.core["privmsg"].main(connection, line.split()[2], "\002[%s]\017 %s - \002Aprox: %s\017 - http://www.identi.ca/notice/%s" % (name, last, time, identification))
+			connection.core["privmsg"].main(connection, line.split()[2], "Sorry, they haven't posted on identi.ca")
 	except:
 		traceback.print_exc()
 		connection.core["privmsg"].main(connection, line.split()[2], "An error has occured")
