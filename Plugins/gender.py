fc8b7ee271b0ab53c752cca04aa7b40248199dfb
Jessica T <jessica@megworld.co.uk>
2012-07-25 00:59:00 +0100

    Adds gender plugin

---
+++ Plugins/gender.py
@@ -0,0 +1,39 @@
+##
+#This file is part of MegBot.
+#
+#   MegBot is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   MegBot is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with MegBot.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+import urllib2
+
+def main(connection, line):
+	# Find name
+	if len(line) <= 3:
+		Channel.send("Please specify a name.")
+		return
+	name = line.split()[4]
+	
+	definitions = {
+		"F":"female",
+		"M":"male",
+		"U":"unisex"
+	}
+
+	s = urllib2.urlopen("http://api.megworld.co.uk/Name-Gender/gender.php?name=%s" % name)
+	d = s.read()
+	if d in definitions.keys():
+		Channel.send("%s is %s" % (name, definitions[d]))
+	else:
+		Channel.send("Sorry we don't know %s, please check later for it" % name)
+
