6c4f033be9800e8cc4eae691d1aaca4c4115977a
Jessica T <xray7224@googlemail.com>
2012-08-19 16:37:48 +0000

    Fixes type conversion not occuring

--- Core/privmsg.py
+++ Core/privmsg.py
@@ -1,5 +1,7 @@
 def main(connection, channel, message):
 	""" Sends a PRIVMSG """
+	if type(message) != type(str):
+		message = str(message)
 	connection.core["Coreraw"].main(connection, "PRIVMSG %s :%s" % (channel, message))
 
 def initalise():
