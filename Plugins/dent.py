0eea0048055a2d5ead12a76298e73bec7c49634b
Jessica T <xray7224@googlemail.com>
2012-08-19 17:51:26 +0000

    Uses new library for date

--- Plugins/dent.py
+++ Plugins/dent.py
@@ -17,50 +17,6 @@
 
 import json, urllib2, traceback, time
 
-def ConvertTime(t):
-	# Thu Aug 02 17:16:15 +0000 2012
-	t = " ".join(t.split()[:-2]) + " " + t.split()[-1] # Removes %z due to identi.ca always using UTC and being invalid in time.strptime.
-	t = time.strptime(t, "%a %b %d %H:%M:%S %Y")
-	t = time.time()-time.mktime(t)
-	print "D: %s" % t
-	if t < 60:
-		return "Less than a minute"
-	elif t < 3600:
-		# a hour
-		m = int(t-3600 + .5)
-		if m <= 1:
-			return "A minute"
-		else:
-			return "%s minutes" % m
-	elif t < 36400:
-		# a day
-		h = int(t - 86400 + .5)
-		if h <= 1:
-			return "A hour"
-		else:
-			return "%s hours" % h
-	elif t < 604800:
-		# a week
-		d = int(t - 86400 + .5)
-		if d <= 1:
-			return "A day"
-		else:
-			return "%s days" % d
-	elif t < 2505600:
-		# a month (29, lowest except 28 which rarely happens)
-		w = int(t - 2505600 + .5)
-		if w <= 1:
-			return "A week"
-		else:
-			return "%s weeks" % w
-	else:
-		# a year
-		y = int(t - (time.time()-31536000) + .5)
-		if y <= 1:
-			return "A year"
-		else:
-			return "%s years" % y
-
 def main(connection, line):
 	if len(line.split()) <= 4:
 		line = Raw2Nick(line)
@@ -73,7 +29,7 @@ def main(connection, line):
 			name = data[0]["user"]["screen_name"]
 			cid = data[0]["id"]
 			status = data[0]["text"]
-			time = ConvertTime(data[0]["created_at"])
+			time = Helper.HumanTime(data[0]["created_at"])
 			
 		else:
 			i = urllib2.urlopen("http://identi.ca/api/statusnet/groups/timeline/%s.json" % line.split()[-1])
@@ -83,7 +39,7 @@ def main(connection, line):
 				name = data[0]["user"]["screen_name"]
 				cid = data[0]["id"]
 				status = data[0]["text"]
-				time = ConvertTime(data[0]["created_at"])
+				time = Helper.HumanTime(data[0]["created_at"])
 			else:
 				Channel.send("Sorry, they haven't posted on identi.ca")
 				return
