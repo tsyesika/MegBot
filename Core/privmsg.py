116614d0cedc7b6d7067c85323dcd5c784d22b7c
Jessica Tallon <jess@canddi.com>
2012-11-16 18:51:55 +0000

    @

--- Core/privmsg.py
+++ Core/privmsg.py
@@ -1,5 +1,10 @@
 def main(connection, channel, message):
 	""" Sends a PRIVMSG """
+	if len(message) > 400:
+		to_send = []
+		for x in range((len(messages) % 400) + 1):
+			connection.core["Coreraw"].main(connection, "PRIVMSG %s :%s" (channel, messages[x*400:x*400*2]))
+		
 	if type(message) != type(str):
 		message = str(message)
 	connection.core["Coreraw"].main(connection, "PRIVMSG %s :%s" % (channel, message))
