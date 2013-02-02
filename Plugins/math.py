ca8335ea831673064f44423840b50943c845d0b4
Jessica T <xray7224@googlemail.com>
2013-02-02 12:51:55 +0000

    Adds RPN support for maths plugin

--- Plugins/math.py
+++ Plugins/math.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 ##
 #This file is part of MegBot.
 #
@@ -15,160 +16,149 @@
 #   along with MegBot.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
-import urllib2, urllib, re
+import cmath
 
-# predefined stuff
-currancies = {
-         "Algerian Dinar":"DZD",
-         "Namibian Dollar":"NAD",
-         "Qatari Rial":"QAR",
-         "Egyptian Pound":"EGP",
-         "Bulgarian Lev":"BGN",
-         "Bolivian Boliviano":"BOB",
-         "Danish Krone":"DKK",
-         "Botswanan Pula":"BWP",
-         "Lebanese Pound":"LBP",
-         "Tanzanian Shilling":"TZS",
-         "Vietnamese Dong":"VND",
-         "Malaysian Ringgit":"MYR",
-         "Cayman Islands Dollar":"KYD",
-         "Ukrainian Hryvnia":"UAH",
-         "Jordanian Dinar":"JOD",
-         "Saudi Riyal":"SAR",
-         "Euro":"EUR",
-         "Hong Kong Dollar":"HKD",
-         "Swiss Franc":"CHF",
-         "Salvadoran Coln":"SVC",
-         "Croatian Kuna":"HRK",
-         "Thai Baht":"THB",
-         "Brunei Dollar":"BND",
-         "Uruguayan Peso":"UYU",
-         "Nicaraguan Crdoba":"NIO",
-         "Moroccan Dirham":"MAD",
-         "Philippine Peso":"PHP",
-         "South African Rand":"ZAR",
-         "Paraguayan Guarani":"PYG",
-         "Nigerian Naira":"NGN",
-         "Costa Rican Coln":"CRC",
-         "United Arab Emirates Dirham":"AED",
-         "Estonian Kroon":"EEK",
-         "Sri Lankan Rupee":"LKR",
-         "Slovak Koruna":"SKK",
-         "Pakistani Rupee":"PKR",
-         "Hungarian Forint":"HUF",
-         "Romanian Leu":"RON",
-         "Ugandan Shilling":"UGX",
-         "Jamaican Dollar":"JMD",
-         "Seychellois Rupee":"SCR",
-         "Turkish Lira":"TRY",
-         "Bangladeshi Taka":"BDT",
-         "Yemeni Rial":"YER",
-         "CFA Franc BCEAO":"XOF",
-         "Netherlands Antillean Guilder":"ANG",
-         "Norwegian Krone":"NOK",
-         "Indian Rupee":"INR",
-         "Mexican Peso":"MXN",
-         "Czech Republic Koruna":"CZK",
-         "Nepalese Rupee":"NPR",
-         "Colombian Peso":"COP",
-         "Mauritian Rupee":"MUR",
-         "Indonesian Rupiah":"IDR",
-         "Honduran Lempira":"HNL",
-         "Fijian Dollar":"FJD",
-         "Peruvian Nuevo Sol":"PEN",
-         "US Dollar":"USD",
-         "Israeli New Sheqel":"ILS",
-         "Dominican Peso":"DOP",
-         "Moldovan Leu":"MDL",
-         "Swedish Krona":"SEK",
-         "Zambian Kwacha":"ZMK",
-         "Maldivian Rufiyaa":"MVR",
-         "Australian Dollar":"AUD",
-         "South Korean Won":"KRW",
-         "Venezuelan Bolvar":"VEF",
-         "Chilean Peso":"CLP",
-         "Lithuanian Litas":"LTL",
-         "Kazakhstani Tenge":"KZT",
-         "Russian Ruble":"RUB",
-         "Trinidad and Tobago Dollar":"TTD",
-         "Omani Rial":"OMR",
-         "Brazilian Real":"BRL",
-         "Polish Zloty":"PLN",
-         "Kenyan Shilling":"KES",
-         "Macedonian Denar":"MKD",
-         "British Pound Sterling":"GBP",
-         "New Taiwan Dollar":"TWD",
-         "Canadian Dollar":"CAD",
-         "Kuwaiti Dinar":"KWD",
-         "Papua New Guinean Kina":"PGK",
-         "Singapore Dollar":"SGD",
-         "Uzbekistan Som":"UZS",
-         "Chinese Yuan":"CNY",
-         "Sierra Leonean Leone":"SLL",
-         "Tunisian Dinar":"TND",
-         "New Zealand Dollar":"NZD",
-         "Latvian Lats":"LVL",
-         "Argentine Peso":"ARS",
-         "Serbian Dinar":"RSD",
-         "Bahraini Dinar":"BHD",
-         "Japanese Yen":"JPY"
-}
-currancy_switch = {}
-for c in currancies:
-    currancy_switch[currancies[c].lower()] = c
-for x in currancies.keys():
-    currancies[x.lower()] = currancies[x]
-    del currancies[x]
+base = 10 
 
+class TypeError(Exception):
+    pass
+class MathsError(Exception):
+    pass
 
-def fixurl(url):
-    url = url.replace("+", "%2B")
-    url = url.replace("/", "%2F")
-    url = url.replace("(", "%28")
-    url = url.replace(")", "%29")
-    url = url.replace(" ", "+")
-    return url
+class Elem():
+    OPERAND = 0 # lets use this as an operand ID
+    OPERATOR = 1 # lets use this as the operator ID
+    MFUNC = 2 # this is used for maths functins (sqrt, log, etc...)
+    def __init__(self, value, value_type):
+        self.value = value
 
-def main(connection, line):
-    global currancy_switch, currancies
-    if not Info.args:
-        Channel.send("Plese enter mathamatical expression.")
-        return
-    exp = " ".join(Info.args)
-    lexp = exp.lower()
-    # Try and figure out what it is first...
-    # Conversion?
-    if lexp.find("to")!=-1:
-        #currancy?
-        tpart = lexp.split("to")
-        a, tpart[0]  = tuple(tpart[0].split(" ")[:2])
-        tpart = [tpart[0].replace(" ", ""), tpart[1].replace(" ", "")]
-        if (tpart[0] in currancies.keys()):
-            tpart[0] = currancies[tpart[0]].lower()
-        if (tpart[1] in currancies.keys()):
-            tpart[1] = currancies[tpart[1]].lower()
-        if (tpart[0] in currancy_switch.keys()) and (tpart[1] in currancy_switch.keys()):
-            google = urllib2.Request("http://www.google.com/finance/converter?a=%s&from=%s&to=%s" % (a, tpart[0].upper(), tpart[1].upper()))
-            google.add_header("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel; Mac OS X 10_6_6; en-gb) AppleWebKit/533.19.4 (KHTML, like Gecko Vesion/5.0.3")
-            google = urllib2.urlopen(google)
-            source = google.read()
-            total = re.findall("<div id=currency_converter_result>(.+?)<span class=bld>(.+?)</span>", source)[0][1]
-            Channel.send("%s %s = %s %s" % (a, currancy_switch[tpart[0]], total.split(" ")[0], currancy_switch[tpart[1]]))
+        # validate type
+        if value_type == self.OPERATOR or value_type == self.OPERAND:
+            self.type = value_type
+        else:
+            raise TypeError(value_type)
+    def calculate(self, operand1, operand2):
+        """ Calculates operand1 <self> operand2 """
+        if self.value == "+":
+            return operand1 + operand2
+        elif self.value == "-":
+            return operand1 - operand2
+        elif self.value == "*":
+            return operand1 * operand2
+        elif self.value == "/":
+            return operand1 / operand2
+        elif self.value == "^":
+            return operand1 ** operand2
+        else:
+            # what else?
+            Channel.send("Something has gone wrong! Sorry. (%s %s %s)"
+                    % (operand1, self.value, operand2))
+            raise MathsError("%s %s %s" 
+                        % (operand1, self.value, operand2))
+
+def display(value):
+    """ Displays the value """
+    answer = value
+    if int(value) == value:
+        answer = int(value)
+    Channel.send("Answer: %s" % answer)
+
+def prefix(equation):
+    """ Handles PN maths """
+    Channel.send("Sorry, we don't currently support prefix (pn) notation")
+
+def postfix(equation):
+    """ Handles RPN maths """
+    stack = []
+    for elem in equation:
+        # okay so if we've got two values in stack we expect
+        # an operator, if not, we expect a value to add to
+        # the stack
+        slen = len(stack)
+        if slen < 2 and elem.type == elem.OPERAND:
+            stack.append(elem.value)
+        elif slen == 2 and elem.type == elem.OPERATOR:
+            try:
+                stack = [elem.calculate(stack[0], stack[1])]
+            except MathsError:
+                return
+        else:
+            Channel.send("Maths error (Stack: %s, value: %s)" % (stack, elem.value))
             return
+    display(stack[0])
 
-    google = urllib2.Request("http://google.com/m?q=%s" % fixurl(exp))
-    google.add_header("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-gb) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3")
-    google = urllib2.urlopen(google)
-    source = google.read()[5000:]
-    #Channel.send("%s" % source[source.find("999 + 999")-30:source.find("2 + 2 = 4")+20])
-    #f = open("test.html", "w")
-        #f.write(source)
-        #f.close()
-    a = re.findall("<span class=\"sifhoi\">(.+?)</span> </d", source)
-    a = a[0].replace("&nbsp;", ",")
-    try:
-        Channel.send("%s" % a)
-    except:
-        Channel.send("Math Error: ?")
+def infix(equation):
+    """ Handles infix notation """
+    Channel.send("Sorry, we don't currently support infix notation.")
 
+def main(conneciton, info):
+    """ Calculates the answer to an equation """
+    global base
+    equation = []
+    for item in Info.args:
+        # okay it could be a number of things actually,
+        # hex? 0x<val>
+        # octal o<val> || º<value>
+        # binary? 0<value> (not sure)
+        # Senary? (fyfyh come on...)
+        if item.startswith("0x"):
+            # hex!
+            val = Elem(int(item, 16), Elem.OPERAND)
+            base = 16
+        elif item.startswith("o") or item.startswith("º"):
+            val = Elem(int(item, 8), Elem.OPERAND)
+            base = 8
+        elif item.startswith("0"):
+            # assume binary?
+            pass
+        # okay we need to now look at operators and functions before
+        # we look at base 10
+        elif "+" == item:
+            val = Elem(item, Elem.OPERATOR)
+        elif "-" == item:
+            val = Elem(item, Elem.OPERATOR)
+        elif "*" == item:
+            val = Elem(item, Elem.OPERATOR)
+        elif "/" == item:
+            val = Elem(item, Elem.OPERATOR)
+        elif "^" == item:
+            val = Elem(item, Elem.OPERATOR)
+        # mathmatical functions?
+        elif item.startswith("sqrt"):
+            val = Elem(item, Elem.MFUNC)
+        elif item.startswith("log"):
+            val = Elem(item, Elem.MFUNC)
+        elif item.startswith("lg"):
+            # log to the base 2?
+            val = Elem(item, Elem.MFUNC)
+        elif item.startswith("ln"):
+            # log the base e?
+            val = Elem(item, Elem.MFUNC)
+        elif item.startswith("tan"):
+            val = Elem(item, Elem.MFUNC)
+        elif item.startswith("cos"):
+            val = Elem(item, Elem.MFUNC)
+        elif item.startswith("sin"):
+            val = Elem(item, Elem.MFUNC)
+        # lets have a few constants eh?
+        elif "pi" == item or "π" == item:
+            val = Elem(cmath.pi, Elem.OPERAND)
+        elif "e" == item:
+            val = Elem(cmath.e, Elem.OPERAND)
+        else:
+            # base 10.
+            val = Elem(float(item), Elem.OPERAND)
+
+        # assume nothing has gone pair shaped.
+        equation.append(val)
+
+    if equation[0].type == Elem.OPERATOR:
+        # pn
+        prefix(equation)
+    elif equation[-1].type == Elem.OPERATOR:
+        # rpn
+        postfix(equation)
+    else:
+        # assume infix
+        infix(equation)
 help = "Uses google to do calculations or conversions"
