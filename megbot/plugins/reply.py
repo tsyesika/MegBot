c67c4b5167bd2d9aa944f1c5392f6735d6621be1
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2017-10-01 16:29:18 +0100

    New reply plugin

---
+++ megbot/plugins/reply.py
@@ -0,0 +1,129 @@
+##
+#   This file is part of MegBot.
+#
+#   MegBot is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   MegBot is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with MegBot.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+import random
+import re
+
+
+EVENT_ID = "replyMsg"
+DATABASE_STR = "replies.{}"
+CHATTINESS = 30  # where 100 would be reply all the time
+
+_cache = {}
+
+
+class Reply(object):
+    valid_options = ["reply", "chance"]
+
+    def __init__(self, msg, channel):
+
+        self.channel = channel
+        self.regex, self.regex_compiled, self.options = self.parse_msg(msg)
+
+    def parse_msg(self, msg):
+        # msg = "cloud<reply>butt<chance>999"
+        msg_split = msg.split("<")
+
+        regex = msg_split[0]
+        options = {}
+        for opt in msg_split[1:]:
+            key, value = opt.split(">")
+            if key not in self.valid_options:
+                raise Exception("{} is not a valid option".format(key))
+            options[key] = value
+
+        if "reply" not in options:
+            raise Exception("Specify a reply")
+
+        regex_compiled = re.compile(regex)
+
+        return regex, regex_compiled, options
+
+    def serialize(self):
+        output = [self.regex]
+        for k, v in self.options.items():
+            output.append("<{}>{}".format(k, v))
+
+        return "".join(output), self.channel
+
+    def sub(self, info):
+        if info.channel_name != self.channel:
+            return
+
+        chance = self.options.get("chance", None)
+
+        if chance is None:
+            chance = CHATTINESS * 0.01
+        else:
+            chance = int(chance) * CHATTINESS * 0.0001
+
+        rand = random.random()
+        if rand < chance:
+            if self.regex_compiled.search(info.message):
+                return self.options["reply"]
+
+
+def main(connection, info):
+    store = connection.libraries["store"]
+
+    try:
+        msg = " ".join(info.args)
+        reply_obj = Reply(msg, info.channel_name)
+    except Exception:
+        info.channel.send(u"I understand... not")
+
+    db_name = DATABASE_STR.format(connection.name)
+    replies = store.Store(db_name)
+    replies[reply_obj.regex] = reply_obj.serialize()
+    replies.save()
+    _cache[reply_obj.regex] = reply_obj
+    info.channel.send(u"I understand")
+
+
+def on_PRIVMSG(connection, info):
+    for reply in _cache.values():
+        msg = reply.sub(info)
+        if msg is not None:
+            info.channel.send(msg)
+
+
+def init(connection):
+    store = connection.libraries["store"]
+    db_name = DATABASE_STR.format(connection.name)
+    try:
+        replies = store.Store(db_name)
+    except IOError:
+        replies = store.Store(db_name, {})
+        replies.save()
+
+    for reply in replies.values():
+        reply_obj = Reply(*reply)
+        _cache[reply_obj.regex] = reply_obj
+
+    info = connection.libraries["IRCObjects"].Info()
+
+    # this will peform an event on privmsg.
+    info.action = "PRIVMSG"
+    event = connection.core["Corehandler"].IRCEvent(info, on_PRIVMSG, EVENT_ID)
+    connection.handler.register_event(event)
+
+
+def unload(connection):
+    connection.handler.unregister_event(EVENT_ID)
+
+
+help = u"regex<reply>reply<chance>100"
