ba24f446bdd4d3f8ad4f3521033cf537fd8b3741
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2019-09-09 01:44:27 +0100

    Rework reply plugin
    - replies are now removable
    - there can be multiple replies with the same regex
    - changes to the database are now locked
    - now accepts the <flags> option
    - logs errors to console
    - !shutup command to quieten bot
    - remove beertime plugin, we can recreate this with the reply plugin now

--- megbot/plugins/reply.py
+++ megbot/plugins/reply.py
@@ -15,20 +15,29 @@
 #   along with MegBot.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
+from datetime import datetime, timedelta
+from threading import RLock
+import logging
 import random
 import re
 
+_log = logging
 
 EVENT_ID = "replyMsg"
 DATABASE_STR = "replies.{}"
 CHATTINESS = 30  # where 100 would be reply all the time
+SHUTUP_SECONDS = 30 * 60  # how long to shutup for
 
-_cache = {}
-
+_cache = []
 _what_you_said = {}
+_lock = RLock()
+
+_shutup = {}
+_shutup_lock = RLock()
+
 
 class Reply(object):
-    valid_options = ["reply", "chance"]
+    valid_options = ["reply", "chance", "flags"]
 
     def __init__(self, msg, channel):
 
@@ -50,7 +59,10 @@ class Reply(object):
         if "reply" not in options:
             raise Exception("Specify a reply")
 
-        regex_compiled = re.compile(regex)
+        flags = re.UNICODE
+        if "i" in options.get("flags", ""):
+            flags = flags | re.IGNORECASE
+        regex_compiled = re.compile(regex, flags=flags)
 
         return regex, regex_compiled, options
 
@@ -78,67 +90,161 @@ class Reply(object):
                 return self.options["reply"]
 
 
-def main(connection, info):
+def open_database(connection):
     store = connection.libraries["store"]
+    db_name = DATABASE_STR.format(connection.name)
+    try:
+        replies = store.Store(db_name)
+        replies["rules"]
+    except (IOError, KeyError):
+        replies = store.Store(db_name, {"rules": []})
+        with _lock:
+            replies.save()
 
+    return replies
+
+
+def main(connection, info):
     try:
         msg = u" ".join(info.args)
         reply_obj = Reply(msg, info.channel_name)
     except Exception:
+        _log.exception("Could not parse reply")
         info.channel.send(u"I understand... not")
         return
 
-    db_name = DATABASE_STR.format(connection.name)
-    replies = store.Store(db_name)
-    replies[reply_obj.regex] = reply_obj.serialize()
-    replies.save()
-    _cache[reply_obj.regex] = reply_obj
-    info.channel.send(u"I understand")
+    with _lock:
+        idx = len(_cache)
+        replies = open_database(connection)
+        replies["rules"].append(reply_obj.serialize())
+        err = False
+        try:
+            replies.save()
+        except Exception:
+            # don't send data while in a lock, set this and send out error
+            # message later
+            err = True
+            _log.exception("Couldn't save replies database")
+        else:
+            _cache.append(reply_obj)
+
+    if err:
+        info.channel.send(u"I understand... but I can't save that to my database")
+    else:
+        info.channel.send(u"I understand ({})".format(idx))
 
 
 def on_PRIVMSG(connection, info):
-    for reply in _cache.values():
+    key = "{}#{}".format(connection.name, info.channel_name)
+    with _shutup_lock:
+        if key in _shutup:
+            if datetime.utcnow() > _shutup[key]:
+                del _shutup[key]
+            else:
+                return
+
+    for reply in _cache:
         msg = reply.sub(info)
         if msg is not None:
             info.channel.send(msg)
             key = "{}#{}".format(connection.name, info.channel_name)
             _what_you_said[key] = reply
+            return
 
 
 def on_what_now(connection, info):
     key = "{}#{}".format(connection.name, info.channel_name)
-    if key in _what_you_said:
-        msg = u"{}: reply was based on {}".format(info.nick, _what_you_said[key].serialize()[0])
-        info.channel.send(msg)
-    else:
+    try:
+        with _lock:
+            reply = _what_you_said[key]
+            try:
+                idx = u"({}) ".format(_cache.index(reply))
+            except ValueError:
+                idx = u""
+            msg = u"{}: reply was based on {}{}".format(info.nick, idx, reply.serialize()[0])
+    except KeyError:
         info.channel.send(u"I didn't say anything")
+    else:
+        info.channel.send(msg)
+
+
+def on_remove_reply(connection, info):
+    error = False
+    try:
+        if len(info.args) != 1:
+            raise TypeError
+
+        idx = int(info.args[0])
+    except (TypeError, IndexError, ValueError):
+        info.channel.send(u"Cannot remove that")
+        return
+
+    with _lock:
+        replies = open_database(connection)
+        try:
+            _cache[idx]
+        except IndexError:
+            error = True
+        else:
+            del _cache[idx]
+            del replies["rules"][idx]
+            try:
+                replies.save()
+            except Exception:
+                _log.exception("Couldn't save replies database")
+    if error:
+        info.channel.send(u"Something went wrong.")
+    else:
+        info.channel.send(u"{} deleted".format(idx))
+
+
+def on_shutup(connection, info):
+    key = "{}#{}".format(connection.name, info.channel_name)
+    with _shutup_lock:
+        if key in _shutup:
+            _shutup[key] += timedelta(seconds=SHUTUP_SECONDS)
+        else:
+            _shutup[key] = datetime.utcnow() + timedelta(seconds=SHUTUP_SECONDS)
+    info.channel.send("Shutting up until {}".format(_shutup[key].strftime("%Y-%m-%d %H:%M:%S")))
 
 
 def init(connection):
-    store = connection.libraries["store"]
-    db_name = DATABASE_STR.format(connection.name)
-    try:
-        replies = store.Store(db_name)
-    except IOError:
-        replies = store.Store(db_name, {})
-        replies.save()
+    with _lock:
+        replies = open_database(connection)
 
-    for reply in replies.values():
-        reply_obj = Reply(*reply)
-        _cache[reply_obj.regex] = reply_obj
+        for reply in replies["rules"]:
+            reply_obj = Reply(*reply)
+            _cache.append(reply_obj)
 
-    info = connection.libraries["IRCObjects"].Info()
-
-    # this will peform an event on privmsg.
-    info.action = "PRIVMSG"
-    event = connection.core["Corehandler"].IRCEvent(info, on_PRIVMSG, EVENT_ID)
+    # listens for matching messages
+    listen = connection.libraries["IRCObjects"].Info()
+    listen.action = "PRIVMSG"
+    event = connection.core["Corehandler"].IRCEvent(listen, on_PRIVMSG, EVENT_ID)
     connection.handler.register_event(event)
 
-    info = connection.libraries["IRCObjects"].Info()
-    info.action = "PRIVMSG"
-    info.plugin_name = "what"
-    info.args = ["was", "that?"]
-    event = connection.core["Corehandler"].IRCEvent(info, on_what_now, EVENT_ID)
+    # !what was that?
+    what = connection.libraries["IRCObjects"].Info()
+    what.action = "PRIVMSG"
+    what.plugin_name = "what"
+    what.args = ["was", "that?"]
+    what.trigger = connection.settings["trigger"]
+    event = connection.core["Corehandler"].IRCEvent(what, on_what_now, EVENT_ID)
+    connection.handler.register_event(event)
+
+    # !removeReply <int>
+    remove = connection.libraries["IRCObjects"].Info()
+    remove.action = "PRIVMSG"
+    remove.plugin_name = "remove"
+    remove.trigger = connection.settings["trigger"]
+    event = connection.core["Corehandler"].IRCEvent(remove, on_remove_reply, EVENT_ID)
+    connection.handler.register_event(event)
+
+    # !shutup
+    shutup = connection.libraries["IRCObjects"].Info()
+    shutup.action = "PRIVMSG"
+    shutup.plugin_name = "shutup"
+    shutup.trigger = connection.settings["trigger"]
+    event = connection.core["Corehandler"].IRCEvent(shutup, on_shutup, EVENT_ID)
     connection.handler.register_event(event)
 
 
